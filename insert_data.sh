1ba0df1a179593ecb7f6788bdbc5a5b81236e967
sqldb-sk <kordic.simeon@gmail.com>
2025-01-12 17:10:21 +0000

    Save progress: 880.1

--- insert_data.sh
+++ insert_data.sh
@@ -4,6 +4,8 @@
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
+echo $($PSQL "TRUNCATE students,majors,courses,majors_courses")
+
 cat courses_test.csv | while IFS="," read MAJOR COURSE
 do
   if [[ $MAJOR != major ]]
